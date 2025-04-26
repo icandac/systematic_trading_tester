024608ec1c5b76cbfa9fb389dadfdbc74a1a17d6
I. Ceyhun Andac, Ph.D. <icandac@users.noreply.github.com>
2025-04-26 13:18:30 +0200

    Github ci (#1)
    * github ci is added with some small docstring changes in many files
    
    * sklearn ci issue solver
    
    * constraints.txt name change
    
    * constraints.txt is removed, a workaround is used to handle the backtester sklearn issue.
    
    * quick fix of a doubled line on ci
    
    * sample test is added

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -4,6 +4,11 @@ import numpy as np
 def calculate_returns(df: pd.DataFrame, column="close"):
     """
     Calculate log returns or simple returns on 'column'.
+    Args:
+        df (pd.DataFrame): DataFrame containing the price data.
+        column (str): Column name for which to calculate returns.
+    Returns:
+        pd.DataFrame: DataFrame with an additional column for returns.
     """
     df["returns"] = df[column].pct_change().fillna(0)
     return df
@@ -11,6 +16,11 @@ def calculate_returns(df: pd.DataFrame, column="close"):
 def calculate_sharpe_ratio(returns, risk_free_rate=0.00):
     """
     Calculate Sharpe Ratio = (mean(returns) - risk_free_rate) / std(returns)
+    Args:
+        returns (pd.Series): Series of returns.
+        risk_free_rate (float): Risk-free rate, default is 0.00.
+    Returns:
+        float: Sharpe Ratio.
     """
     excess_returns = returns - risk_free_rate
     if np.std(excess_returns) == 0:
