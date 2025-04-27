eabdbfd512edd8361e0bc7a40a32851c255d8705
ica <andac@itu.edu.tr>
2025-04-27 20:29:22 +0200

    one last quick fix in a comment

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "stt = systematic_trading_tester.main:cli_entry",  # you expose a cli()
+            "stt = systematic_trading_tester.main:cli_entry",  # to expose a cli()
         ]
     },
 )
